e3c1fbfcb10fefccb72dadd44432218c1011764c
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-07-22 20:12:15 +0530

    22/7/2022 constructor,this pointer,getter and setter method

---
+++ src/ConstructorDemo.java
@@ -0,0 +1,20 @@
+public class ConstructorDemo {
+
+    // constructor:
+    // spl function (VIP function)
+    // public
+    // automatic calling ( object creation)
+    //same name as class name
+    //no return type
+    // initialisation purpose
+    // always we should inside the same class
+
+    ConstructorDemo(){
+
+    }
+
+    public static void main(String[] args) {
+        ConstructorDemo d = new ConstructorDemo();
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Employee newEmployee = new Employee(2,"revathi","bangl","Hr"); //object creation
+
+String name=newEmployee.getName();
+int id= newEmployee.getId();
+        System.out.println(name);
+        System.out.println(id);
+
+
+    }
+}
